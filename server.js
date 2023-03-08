6e75eb802667fd0434c66acf235f98d8ecc6bed9
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-07 19:38:14 -0600

    se agregaron rutas y servidor y configuracion

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const port = process.env.PORT || 3000;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.disable('x-powered-by');
+
 app.set('port', port);
 
 server.listen(3000, '192.168.1.5' || 'localhost', function(){
     console.log('Aplicacion de nodeJS '+ process.pid + " Iniciada ...........");
+});
+
+app.get('/', (req, res) => {
+    res.send('Ruta raiz del backend');
+});
+
+app.use((err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
 });
