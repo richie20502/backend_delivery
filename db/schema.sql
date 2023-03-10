e868e879175902539440437610604e150a52eed7
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-09 21:48:13 -0600

    modificacion de archivo de sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,30 @@ CREATE TABLE roles(
 	updated_at TIMESTAMP(0) NOT NULL
 );
 
+INSERT INTO roles(
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'CLIENTE',
+	'client/products/list',
+	'2023-01-01',
+	'2023-01-01'
+),
+VALUES(
+	'RESTAURANTE',
+	'restaurant/orders/list',
+	'2023-01-01',
+	'2023-01-01'
+),VALUES(
+	'REPARTIDOR',
+	'delivery/orders/list',
+	'2023-01-01',
+	'2023-01-01'
+);
+
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
 	id BIGSERIAL PRIMARY KEY,
@@ -21,6 +45,16 @@ CREATE TABLE users(
 	session_token VARCHAR(255) NULL,
 	created_at TIMESTAMP(0) NOT NULL,
 	updated_at TIMESTAMP(0) NOT NULL
+);
+DROP TABLE IF EXISTS user_has_roles CASCADE;
+CREATE TABLE user_has_roles(
+	id_user BIGSERIAL NOT NULL,
+	id_rol  BIGSERIAL NOT NULL,
+	created_at TIMESTAMP(0) NOT NULL,
+	updated_at TIMESTAMP(0) NOT NULL,
+	FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(id_rol) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(id_user,id_rol)
 );
 
 
